b0e457126a7140391d099eb2635f325688efb5c9
M3nny <77993658+M3nny@users.noreply.github.com>
2022-03-18 19:05:31 +0100

    Implement home UI

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,9 @@ import 'package:audioplayers/audio_cache.dart';
 import 'package:flashlight/flashlight.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-void main() => runApp(const Human2Morse());
+void main() => runApp(Human2Morse());
 
+// ignore: must_be_immutable
 class Human2Morse extends StatelessWidget {
   const Human2Morse({Key key}) : super(key: key);
 
@@ -15,26 +16,155 @@ class Human2Morse extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Column(
-          children: <Widget>[
-            Expanded(
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: Container(
-                  margin: EdgeInsets.only(top: 70),
-                  width: 380,
-                  child: TextField(
-                    maxLines: 5,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      //hintText: "translate from human",
-                      fillColor: Colors.grey[300],
-                      filled: true,
-                    ),
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                margin: EdgeInsets.only(top: 50),
+                width: 380,
+                child: TextField(
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    //hintText: "translate from human",
+                    fillColor: Color(0xffD8D8D8),
+                    filled: true,
                   ),
                 ),
               ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                margin: EdgeInsets.only(top: 30),
+                width: 380,
+                child: SvgPicture.asset(
+                  "assets/images/swap.svg",
+                  width: 100,
+                  height: 100,
+                  color: Color(0xff969696),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                margin: EdgeInsets.only(top: 30),
+                width: 380,
+                child: TextField(
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    //hintText: "translate from human",
+                    fillColor: Color(0xffD8D8D8),
+                    filled: true,
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      margin: EdgeInsets.only(top: 150),
+                      width: 80,
+                      height: 80,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Colors.grey[300]),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: SizedBox(
+                          width: 380,
+                          child: SvgPicture.asset(
+                            "assets/images/torch.svg",
+                            width: 100,
+                            height: 100,
+                            color: Color(0xff797979),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      margin: EdgeInsets.only(top: 150),
+                      width: 80,
+                      height: 80,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Colors.grey[300]),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: SizedBox(
+                          width: 380,
+                          child: SvgPicture.asset(
+                            "assets/images/vibration.svg",
+                            width: 100,
+                            height: 100,
+                            color: Color(0xff797979),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      margin: EdgeInsets.only(top: 150),
+                      width: 80,
+                      height: 80,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              Colors.grey[300]),
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: SizedBox(
+                          width: 380,
+                          child: SvgPicture.asset(
+                            "assets/images/sound.svg",
+                            width: 100,
+                            height: 100,
+                            color: Color(0xff797979),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
